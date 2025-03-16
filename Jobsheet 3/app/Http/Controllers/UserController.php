6b3d5a7446c8368bf887a740ffacda655fb5a5be
yassnothere <achmadiaz122@gmail.com>
2025-03-16 21:59:44 +0700

    Jobsheet 4 |  Praktikum 2.2

--- Jobsheet 3/app/Http/Controllers/UserController.php
+++ Jobsheet 3/app/Http/Controllers/UserController.php
@@ -10,9 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::find(20, ['username', 'nama'], function (){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
