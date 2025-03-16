69a58c9ab1a536d90edb340c94d3cef086e96436
yassnothere <achmadiaz122@gmail.com>
2025-03-16 22:53:50 +0700

    Jobsheet 4 | Praktikum 2.5

--- Jobsheet 3/app/Http/Controllers/UserController.php
+++ Jobsheet 3/app/Http/Controllers/UserController.php
@@ -10,15 +10,24 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
-        $user->save();
+        $user = UserModel :: create([
+            'username' => 'manager11',
+            'nama' => 'Manager11',
+            'password' => Hash :: make('12345'),
+            'level_id' => 2,
+            
+            ]);
+            
+            $user->username = 'manager12';
+            
+            $user->save();
+            
+            $user->wasChanged(); // true
+            $user->wasChanged('username'); // true
+            $user->wasChanged(['username', 'level_id']); // true
+            $user->wasChanged('nama'); // false
+            dd($user->wasChanged(['nama', 'username'])); // true
+
         return view('user', ['data' => $user]);
     }
 }
